00500179bd765e84c37cacea6c1fd7b4743acaf8
axdiaz <axeldiaz89@gmail.com>
2020-05-26 11:46:31 -0300

    creando el archivo howToMakeATest.js

---
+++ __tests__/howToMakeATest.js
@@ -0,0 +1,8 @@
+describe('algo que describa el conjunto de tests', () => {
+  //https://jestjs.io/docs/en/api#describename-fn
+  //describe se utiliza para armar bloques de tests que estan relacionados entre si.
+  //se pueden anidar describe dentro de otro describe
+
+
+  //https://jestjs.io/docs/en/api#testname-fn-timeout
+})
